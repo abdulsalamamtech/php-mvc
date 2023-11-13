55da68718f8f49154f3d65c7786bb4a7d69c764a
amtech <amtechdigitalnetworks@gmail.com>
2023-11-13 16:11:34 +0100

    UPDATE, REFACTOR updating and refactoring the code

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MVC API</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <a href="https://github.com/abdulsalamamtech/">Developed By AbdulsalamAmtech</a>
+</body>
+</html>
